38ce7b87707d583373104d3963e6590f1f95be53
Hussein <41930483@students.liu.edu.lb>
2024-02-05 21:41:20 +0200

    Add migrations for difficulties, regions, and walks tables

---
+++ NZWALKS/Migrations/20240205191109_nzwalksmigration.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWALKS.Migrations
+{
+    /// <inheritdoc />
+    public partial class nzwalksmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "difficulties",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_difficulties", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "regions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RegionImageURI = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_regions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Walks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LengthInKm = table.Column<double>(type: "float", nullable: false),
+                    WalkImageURI = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RegionId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DifficultyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    regionsId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Walks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Walks_difficulties_DifficultyId",
+                        column: x => x.DifficultyId,
+                        principalTable: "difficulties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Walks_regions_regionsId",
+                        column: x => x.regionsId,
+                        principalTable: "regions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Walks_DifficultyId",
+                table: "Walks",
+                column: "DifficultyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Walks_regionsId",
+                table: "Walks",
+                column: "regionsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Walks");
+
+            migrationBuilder.DropTable(
+                name: "difficulties");
+
+            migrationBuilder.DropTable(
+                name: "regions");
+        }
+    }
+}
