dfdf68bf402e8c3a012eaa9f4299458c3d6ce1b9
Hussein <41930483@students.liu.edu.lb>
2024-02-05 21:41:44 +0200

    Add Walks model class

---
+++ NZWALKS/Models/Walks.cs
@@ -0,0 +1,17 @@
+﻿namespace NZWALKS.Models
+{
+    public class Walks
+    {
+        public Guid Id{get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public string? WalkImageURI { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid DifficultyId { get; set; }
+
+        public Regions regions { get; set; }
+        public Difficulty difficulty { get; set; }
+
+    }
+}
