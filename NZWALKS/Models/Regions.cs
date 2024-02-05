114f0a07aeb887b206bd91ee30e8ec5c6d8a287f
Hussein <41930483@students.liu.edu.lb>
2024-02-05 21:41:39 +0200

    Add Regions model class

---
+++ NZWALKS/Models/Regions.cs
@@ -0,0 +1,11 @@
+﻿namespace NZWALKS.Models
+{
+    public class Regions
+    {
+        public Guid Id  { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string? RegionImageURI{ get; set; }
+
+    }
+}
