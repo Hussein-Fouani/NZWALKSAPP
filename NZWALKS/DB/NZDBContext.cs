97cf1a19a94469144b2114c3b503306fd1b07b01
Hussein <41930483@students.liu.edu.lb>
2024-02-05 21:41:15 +0200

    Add NZDBContext class to handle database operations

---
+++ NZWALKS/DB/NZDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWALKS.Models;
+
+namespace NZWALKS.DB
+{
+    public class NZDBContext:DbContext
+    {
+        public NZDBContext(DbContextOptions option):base(option)
+        {
+
+            
+        }
+        public DbSet<Walks> Walks { get; set; }
+        public DbSet<Regions> regions { get; set; }
+        public DbSet<Difficulty> difficulties { get; set; }
+
+    }
+}
