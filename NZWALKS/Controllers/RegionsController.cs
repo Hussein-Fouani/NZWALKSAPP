dbca33075ce13dad189908d1d4d52ad9286fcc75
Hussein <41930483@students.liu.edu.lb>
2024-02-06 10:58:24 +0200

    Add region retrieval endpoints to RegionsController

--- NZWALKS/Controllers/RegionsController.cs
+++ NZWALKS/Controllers/RegionsController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NZWALKS.DB;
+using NZWALKS.Models;
 
 namespace NZWALKS.Controllers
 {
@@ -7,7 +9,25 @@ namespace NZWALKS.Controllers
     [ApiController]
     public class RegionsController : ControllerBase
     {
+        private readonly NZDBContext nZDB;
 
+        public RegionsController(NZDBContext nZDB)
+        {
+            this.nZDB = nZDB;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var region= nZDB.regions.ToList();
+            return Ok(region);
+        }
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetRegionByID([FromRoute]Guid id)
+        {
+            nZDB.regions.Find(id); 
+        }
+            
     }
 }
  
