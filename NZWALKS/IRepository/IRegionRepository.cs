faaa74eafeaef117ff2fd998c341f231610ab5d9
Hussein <41930483@students.liu.edu.lb>
2024-02-08 09:52:49 +0200

    Add IRegionRepository interface

---
+++ NZWALKS/IRepository/IRegionRepository.cs
@@ -0,0 +1,14 @@
+﻿using NZWALKS.Models;
+using NZWALKS.Models.DTO;
+
+namespace NZWALKS.IRepository
+{
+    public interface IRegionRepository
+    {
+        Task<List<Regions>> GetAllAsync();
+        Task<Regions> GetRegionByID(Guid id);
+        Task<Regions> CreateRegion(AddRegionDTO region);
+        Task<Regions> UpdateRegion(Guid id, UpdateRegionDTO region);
+        Task<Regions> DeleteRegion(Guid id);
+    }
+}
