74760584aee0458bb23022d15d6ef31e710cbde8
Hussein <41930483@students.liu.edu.lb>
2024-02-08 09:53:01 +0200

    Add RegionDTO class

---
+++ NZWALKS/Models/DTO/RegionDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace NZWALKS.Models.DTO
+{
+    public class RegionDTO
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string? RegionImageURI { get; set; }
+    }
+}
