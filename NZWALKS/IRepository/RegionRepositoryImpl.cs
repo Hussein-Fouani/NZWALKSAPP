b918a7d4af722c8aaa933ca079aa71897ee81fe9
Hussein <41930483@students.liu.edu.lb>
2024-02-08 09:52:53 +0200

    Add RegionRepositoryImpl class

---
+++ NZWALKS/IRepository/RegionRepositoryImpl.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWALKS.DB;
+using NZWALKS.Models;
+using NZWALKS.Models.DTO;
+
+namespace NZWALKS.IRepository
+{
+    public class RegionRepositoryImpl : IRegionRepository
+    {
+        private readonly NZDBContext nZDB;
+        public RegionRepositoryImpl(NZDBContext nZDB)
+        {
+            this.nZDB = nZDB;
+        }
+
+        public Task<Regions> CreateRegion(AddRegionDTO region)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Regions> DeleteRegion(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Regions>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Regions> GetRegionByID(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Regions> UpdateRegion(Guid id, UpdateRegionDTO region)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
