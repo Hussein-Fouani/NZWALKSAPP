8db03d4237673ba8e22e612524261e2565814843
Hussein <41930483@students.liu.edu.lb>
2024-02-09 13:48:04 +0200

    Add validation attributes and fix typo in UpdateWalkDTO.cs

--- NZWALKS/Models/DTO/UpdateWalkDTO.cs
+++ NZWALKS/Models/DTO/UpdateWalkDTO.cs
@@ -1,12 +1,26 @@
-﻿namespace NZWALKS.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWALKS.Models.DTO
 {
     public class UpdateWalkDTO
     {
-            public string Name { get; set; }
+        [Required]
+        [MaxLength(30, ErrorMessage = "Name has to have a min of 30  Characters")]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(100, ErrorMessage = "Description has to have a min of 100  Characters")]
         public string Description { get; set; }
-        public double LengthInKm { get; set; }
+        [Required]
+        [MaxLength(30, ErrorMessage = "Start has to have a min of 30  Characters")]
+        public string Region { get; set; }
+        [Required]
+        [MaxLength(30, ErrorMessage = "Start has to have a min of 30  Characters")]
+        public string Difficulty { get; set; }
+        [Required]
+        [Range(0,50)]
+        public double LengthInKM { get; set; }
         public string? WalkImageURI { get; set; }
+        public Guid DiffuclityId { get; set; }
         public Guid RegionId { get; set; }
-        public Guid DifficultyId { get; set; }
     }
 }
