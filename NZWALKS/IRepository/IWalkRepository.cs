dd04cbdb6324e9caa5b8cb9a711aa7f72f96a175
Hussein <41930483@students.liu.edu.lb>
2024-02-09 11:40:39 +0200

    Add IWalkRepository interface

---
+++ NZWALKS/IRepository/IWalkRepository.cs
@@ -0,0 +1,17 @@
+﻿using NZWALKS.Models;
+using NZWALKS.Models.DTO;
+using NZWALKS.Models.WalksDTO;
+
+namespace NZWALKS.IRepository
+{
+    public interface IWalkRepository
+    {
+      Task<Walks>  CreateAsync(Walks walkDTO);
+        Task<Walks>  UpdateAsync(UpdateWalkDTO walkDTO,Guid Id);
+        Task<List<Walks>> GetAllWalks();
+        Task<Walks?> GetWalkByIDAsync(Guid id);
+        Task<Walks?> DeleteWalk(Guid id);
+    }
+
+    
+}
