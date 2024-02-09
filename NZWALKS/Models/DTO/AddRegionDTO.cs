daf856e8b824b4cb234943eeaef93c0021da32a0
Hussein <41930483@students.liu.edu.lb>
2024-02-09 13:41:03 +0200

    Add validation attributes to AddRegionDTO class

--- NZWALKS/Models/DTO/AddRegionDTO.cs
+++ NZWALKS/Models/DTO/AddRegionDTO.cs
@@ -1,8 +1,15 @@
-﻿namespace NZWALKS.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWALKS.Models.DTO
 {
     public class AddRegionDTO
     {
+        [Required]
+        [MinLength(2,ErrorMessage ="Code has to have a min of 2 Characters")]
+        [MaxLength(3,ErrorMessage = "Code has to have a max of 3 Characters")] 
         public string Code { get; set; }
+        [Required]
+        [MaxLength(30,ErrorMessage ="Name has to have a min of 30  Characters")]
         public string Name { get; set; }
         public string? RegionImageURI { get; set; }
     }
