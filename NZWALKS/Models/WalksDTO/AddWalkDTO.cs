f0c6cdc71ceb7bfbcdf6c4598b330a5d0c5f823d
Hussein <41930483@students.liu.edu.lb>
2024-02-09 11:41:16 +0200

    Add AddWalkDTO class to WalksDTO namespace

---
+++ NZWALKS/Models/WalksDTO/AddWalkDTO.cs
@@ -0,0 +1,15 @@
+﻿ namespace NZWALKS.Models.WalksDTO
+{
+    public class AddWalkDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Region { get; set; }
+        public string Difficulty { get; set; }
+        public string? WalkImageURI { get; set; }
+        public Guid DiffuclityId { get; set; }
+        public Guid RegionId { get; set; }
+
+
+    }
+}
