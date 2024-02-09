b286d02f4ad55f69fbd981aa27da0d6b66223319
Hussein <41930483@students.liu.edu.lb>
2024-02-09 14:18:21 +0200

    Add ValidateModelAttribute class to NZWALKS.CustomAFilter

---
+++ NZWALKS/CustomAFilter/ValidateModelAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace NZWALKS.CustomAFilter
+{
+    public class ValidateModelAttribute:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+            if (context.ModelState.IsValid == false)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+                
+            }
+        }
+    }
+}
