8b3236fc4b324593ff8197a560108109cc7f20da
Hussein <41930483@students.liu.edu.lb>
2024-02-09 13:48:10 +0200

    Add validation attributes to AddWalkDTO class

--- NZWALKS/Models/WalksDTO/AddWalkDTO.cs
+++ NZWALKS/Models/WalksDTO/AddWalkDTO.cs
@@ -1,11 +1,22 @@
-﻿ namespace NZWALKS.Models.WalksDTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWALKS.Models.WalksDTO
 {
     public class AddWalkDTO
     {
+        [Required]
+        [MaxLength(30, ErrorMessage = "Name has to have a min of 30  Characters")]
         public string Name { get; set; }
+        [Required]
+        [MaxLength(100, ErrorMessage = "Description has to have a min of 100  Characters")]
         public string Description { get; set; }
+        [Required]
+        [MaxLength(30, ErrorMessage = "Start has to have a min of 30  Characters")]
         public string Region { get; set; }
+        [Required]
+        [MaxLength(30, ErrorMessage = "Start has to have a min of 30  Characters")]
         public string Difficulty { get; set; }
+        public double LengthInKM { get; set; }
         public string? WalkImageURI { get; set; }
         public Guid DiffuclityId { get; set; }
         public Guid RegionId { get; set; }
