488ea814f60fda4a1f7cacc44f6364a4bce3a866
Hussein <41930483@students.liu.edu.lb>
2024-02-09 11:40:31 +0200

    Add WalkController with CRUD operations

---
+++ NZWALKS/Controllers/WalkController.cs
@@ -0,0 +1,77 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWALKS.IRepository;
+using NZWALKS.Models;
+using NZWALKS.Models.DTO;
+using NZWALKS.Models.WalksDTO;
+
+namespace NZWALKS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalkController : ControllerBase
+    {
+        private readonly IMapper mapper;
+        private readonly IWalkRepository walkrepository;
+
+        public WalkController(IMapper mapper,IWalkRepository walkrepository)
+        {
+            this.mapper = mapper;
+            this.walkrepository = walkrepository;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateWalks([FromBody] AddWalkDTO walkDTO){
+            var walk = mapper.Map<Walks>(walkDTO);
+            if (walk == null)
+            {
+                return null;
+            }
+           await walkrepository.CreateAsync(walk);
+            return Ok(mapper.Map<WalkDTO>(walk));
+
+        }
+        [HttpGet]
+        // Get Walk
+        public async Task<IActionResult> GetAllWalks()
+        {
+            var walksdomain = walkrepository.GetAllWalks();
+            return Ok(mapper.Map<WalkDTO>(walksdomain));
+
+        }
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult?> GetWalkByID(Guid id)
+        {
+            var walk = walkrepository.GetWalkByIDAsync(id);
+            if (walk == null)
+            {
+                return NotFound();
+            }
+            return Ok(mapper.Map<WalkDTO>(walk));
+        }
+        [HttpPut]
+        [Route("{i d:Guid}")]
+        public async Task<IActionResult> UpdateWalk([FromRoute]Guid id, [FromBody] UpdateWalkDTO walkDTO)
+        {
+            var walk = walkrepository.UpdateAsync(walkDTO, id);
+            if (walk == null)
+            {
+                return NotFound();
+            }
+            return Ok(mapper.Map<WalkDTO>(walk));
+        }
+        [HttpDelete]
+        [Route("{Id:Guid}")]
+        public async Task<IActionResult> DeleteWalk([FromRoute]Guid Id)
+        {
+            var walktodelete = walkrepository.DeleteWalk(Id);
+            if (walktodelete == null)
+            {
+                return NotFound();
+            }
+            return Ok(mapper.Map<WalkDTO>(walktodelete));
+        }
+
+    }
+}
