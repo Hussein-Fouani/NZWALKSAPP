68f1d829e2d9dd8d9b310907f5513d1a821d9811
Hussein <41930483@students.liu.edu.lb>
2024-02-09 11:41:11 +0200

    Add UpdateWalkDTO class

---
+++ NZWALKS/Models/DTO/UpdateWalkDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace NZWALKS.Models.DTO
+{
+    public class UpdateWalkDTO
+    {
+            public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public string? WalkImageURI { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid DifficultyId { get; set; }
+    }
+}
