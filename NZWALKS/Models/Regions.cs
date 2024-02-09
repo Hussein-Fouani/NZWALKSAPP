d46c90c3d2561c6b1f33d789449237cde0bf9fe7
Hussein <41930483@students.liu.edu.lb>
2024-02-09 18:26:02 +0200

    Update RegionImageURI property in Regions.cs

--- NZWALKS/Models/Regions.cs
+++ NZWALKS/Models/Regions.cs
@@ -5,7 +5,7 @@
         public Guid Id  { get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
-        public string? RegionImageURI{ get; set; }
+        public string? RegionImageURI{ get; set; } 
 
     }
 }
