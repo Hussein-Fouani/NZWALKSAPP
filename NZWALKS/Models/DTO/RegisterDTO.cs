660b2248c3489e5e2869d895e14394f80c9033e3
Hussein <41930483@students.liu.edu.lb>
2024-02-10 22:24:16 +0200

    Add RegisterDTO class for user registration

---
+++ NZWALKS/Models/DTO/RegisterDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWALKS.Models.DTO;
+
+public class RegisterDTO
+{
+    [Required]
+    [StringLength(20, MinimumLength = 8)]
+    [DataType(DataType.EmailAddress)]
+    public string UserName { get; set; }
+
+    [Required]
+    [Range(15, 40)]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    public string[] Roles { get; set; }
+}
