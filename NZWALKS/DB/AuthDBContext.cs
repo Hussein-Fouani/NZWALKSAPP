5ab9c4ba13beb28e226e839e21b9718c3fd2a7d0
Hussein <41930483@students.liu.edu.lb>
2024-02-10 21:59:46 +0200

    Update AuthDBContext.cs with role changes

--- NZWALKS/DB/AuthDBContext.cs
+++ NZWALKS/DB/AuthDBContext.cs
@@ -13,20 +13,35 @@ namespace NZWALKS.DB
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            var readerId= "f1086838-8f02-47a4-ac0b-76fdfffad321";
+            var readerRoleID= "f1086838-8f02-47a4-ac0b-76fdfffad321";
             var writerId = "9f92d94f-c288-4152-a453-2208ea6d6c91";
-
             var roles = new List<IdentityRole>
             {
 
                 new IdentityRole
                 {
-                    Id= readerId,
-                    ConcurrencyStamp=writerId,
+                    Id= readerRoleID,
+                    ConcurrencyStamp=readerRoleID,
                     Name="Reader",
                     NormalizedName="Reader".ToUpper(),
-                }
-            };
+                },
+                new IdentityRole
+                {
+                    Id= writerId,
+                    ConcurrencyStamp = writerId,
+                    Name="Writer",
+                    NormalizedName="Writer".ToUpper()
+
         }
+              
+    };
+            builder.Entity<IdentityRole>().HasData(roles);
+
+
+
+}    
+        
     }
+    
 }
+
