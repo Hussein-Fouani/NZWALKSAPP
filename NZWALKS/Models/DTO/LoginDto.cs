6c48dcdf79b07e977e884ab96b6802b2ad3a1c8b
Hussein <41930483@students.liu.edu.lb>
2024-02-10 22:24:12 +0200

    Add LoginDto class for user login functionality

---
+++ NZWALKS/Models/DTO/LoginDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWALKS.Models.DTO;
+
+public class LoginDto
+{
+    [Required]
+    [DataType(DataType.EmailAddress)]
+    public string UserName { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
