b6909e26fa09648e6301326520bd906f131d13df
Hussein <41930483@students.liu.edu.lb>
2024-02-10 09:56:02 +0200

    Add authorization to RegionsController

--- NZWALKS/Controllers/RegionsController.cs
+++ NZWALKS/Controllers/RegionsController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using NZWALKS.Models.DTO;
 
 namespace NZWALKS.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class RegionsController : ControllerBase
