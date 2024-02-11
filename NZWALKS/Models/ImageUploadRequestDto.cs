812e0eeb6bb3a4a14de0978aba92e8fc3c376cf7
Hussein <41930483@students.liu.edu.lb>
2024-02-11 19:35:39 +0200

    Add ImageUploadRequestDto model

---
+++ NZWALKS/Models/ImageUploadRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWALKS.Models
+{
+    public class ImageUploadRequestDto
+    {
+        [Required]
+        public FormFile File { get; set; }
+        [Required]
+        public string FileName { get; set; }
+        public string? FileDescription { get; set; }
+    }
+}
