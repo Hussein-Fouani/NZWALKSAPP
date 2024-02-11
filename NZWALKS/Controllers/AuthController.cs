994047f530d721e9dbd9556314af6aa74eb672d0
Hussein <41930483@students.liu.edu.lb>
2024-02-11 19:33:50 +0200

    Add token repository and implement login functionality

--- NZWALKS/Controllers/AuthController.cs
+++ NZWALKS/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using NZWALKS.IRepository;
 using NZWALKS.Models.DTO;
 using RegisterDTO = NZWALKS.Models.DTO.RegisterDTO;
 
@@ -8,10 +9,12 @@ namespace NZWALKS.Controllers;
 public class AuthController : ControllerBase
 {
     private readonly UserManager<IdentityUser> _userManager;
+    private readonly ITokenRepository _tokenRepository;
 
-    public AuthController(UserManager<IdentityUser> userManager)
+    public AuthController(UserManager<IdentityUser> userManager,ITokenRepository tokenRepository)
     {
         _userManager = userManager;
+        _tokenRepository = tokenRepository;
     }
 
     // GET
@@ -38,13 +41,27 @@ public class AuthController : ControllerBase
 
     [HttpPost]
     [Route("Login")]
-    public async Task<IActionResult> Login([FromBody] LoginDto loginDto )
+    public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
     {
-        var user =await _userManager.FindByEmailAsync(loginDto.UserName );
+        var user = await _userManager.FindByEmailAsync(loginDto.UserName);
         if (user != null)
         {
-            if (user.PasswordHash != null)
-                await _userManager.ChangePasswordAsync(user, user.PasswordHash, loginDto.Password);
+            var checkResult = await _userManager.CheckPasswordAsync(user, loginDto.Password);
+            if (checkResult)
+            {
+                var role = await _userManager.GetRolesAsync(user);
+                if (role.Any())
+                {
+                   var jwtToken= _tokenRepository.CreateToken(user,role.ToList( ));
+                   var response = new LoginResponseDto()
+                   {
+                       JwtToken = jwtToken
+                   };
+                    return Ok(response);
+                }
+                
+            }
+
             return Ok();
         }
 
