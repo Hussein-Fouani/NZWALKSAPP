d3beb98b483ba511622fdbc8aa26d27e0ec30766
Hussein <41930483@students.liu.edu.lb>
2024-02-11 19:35:36 +0200

    Add Image model class

---
+++ NZWALKS/Models/Image.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace NZWALKS.Models;
+
+public class Image
+{
+    public Guid Id { get; set; }
+    [NotMapped]
+    public required IFormFile File { get; set; }
+
+    public string FileName { get; set; }
+    public string? FileDescription { get; set; }
+    public string FileExtension { get; set; }
+    public long FileSizeInBytes { get; set; }
+    public string filePath { get; set; }
+    
+}
