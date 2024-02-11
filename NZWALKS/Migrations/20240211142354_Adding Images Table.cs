9161616c277a9f6edf68a6e87fbbd599e3d05ca2
Hussein <41930483@students.liu.edu.lb>
2024-02-11 19:35:20 +0200

    Add Images Table and Update Table Names

---
+++ NZWALKS/Migrations/20240211142354_Adding Images Table.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWALKS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingImagesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Walks_difficulties_DifficultyId",
+                table: "Walks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Walks_regions_regionsId",
+                table: "Walks");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_regions",
+                table: "regions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_difficulties",
+                table: "difficulties");
+
+            migrationBuilder.RenameTable(
+                name: "regions",
+                newName: "Regions");
+
+            migrationBuilder.RenameTable(
+                name: "difficulties",
+                newName: "Difficulties");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Regions",
+                table: "Regions",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Difficulties",
+                table: "Difficulties",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FileName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FileDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FileExtension = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FileSizeInBytes = table.Column<long>(type: "bigint", nullable: false),
+                    filePath = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Walks_Difficulties_DifficultyId",
+                table: "Walks",
+                column: "DifficultyId",
+                principalTable: "Difficulties",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Walks_Regions_regionsId",
+                table: "Walks",
+                column: "regionsId",
+                principalTable: "Regions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Walks_Difficulties_DifficultyId",
+                table: "Walks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Walks_Regions_regionsId",
+                table: "Walks");
+
+            migrationBuilder.DropTable(
+                name: "Images");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Regions",
+                table: "Regions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Difficulties",
+                table: "Difficulties");
+
+            migrationBuilder.RenameTable(
+                name: "Regions",
+                newName: "regions");
+
+            migrationBuilder.RenameTable(
+                name: "Difficulties",
+                newName: "difficulties");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_regions",
+                table: "regions",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_difficulties",
+                table: "difficulties",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Walks_difficulties_DifficultyId",
+                table: "Walks",
+                column: "DifficultyId",
+                principalTable: "difficulties",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Walks_regions_regionsId",
+                table: "Walks",
+                column: "regionsId",
+                principalTable: "regions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
