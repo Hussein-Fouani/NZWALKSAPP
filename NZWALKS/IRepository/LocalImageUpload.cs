1f88f46443f3c297ec3640608404ca63bc5edf4d
Hussein <41930483@students.liu.edu.lb>
2024-02-11 19:34:24 +0200

    Add LocalImageUpload class for image uploading

---
+++ NZWALKS/IRepository/LocalImageUpload.cs
@@ -0,0 +1,34 @@
+﻿using NZWALKS.DB;
+using NZWALKS.Models;
+
+namespace NZWALKS.IRepository
+{
+    public class LocalImageUpload : IImageRepository
+    {
+        private readonly IWebHostEnvironment ev;
+        private readonly IHttpContextAccessor contextAccessor;
+        private readonly NzdbContext dBContext;
+
+        public LocalImageUpload(IWebHostEnvironment ev,IHttpContextAccessor contextAccessor,NzdbContext dBContext)
+        {
+            this.ev = ev;
+            this.contextAccessor = contextAccessor;
+            this.dBContext = dBContext;
+        }
+        public async Task<Image> UploadImage(Image image)
+        {
+            var localPath = Path.Combine(ev.ContentRootPath, "Images",$"{image.FileName}{image.FileExtension}" );
+            using var stream = new FileStream(localPath, FileMode.Create);
+            await image.File.CopyToAsync(stream);
+
+            var urnfieldPath = $"{contextAccessor.HttpContext.Request.Scheme}//{contextAccessor.HttpContext.Request.Host}{contextAccessor.HttpContext.Request.PathBase}/ Image/{image.FileName}{image.FileExtension}";
+
+            await dBContext.Images.AddAsync(image);
+            await dBContext.SaveChangesAsync();
+            return image;
+
+            
+
+        }
+    }
+}
