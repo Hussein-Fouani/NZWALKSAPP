8ae64f96919e15b28d2fe919c278a397c4a921da
Hussein <41930483@students.liu.edu.lb>
2024-02-21 21:22:19 +0200

    Add GlobalHandler class for handling exceptions

---
+++ NZWALKS/Exceptions/GlobalHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWALKS.Exceptions;
+
+public class GlobalHandler
+{
+    private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+    private readonly RequestDelegate _requestDelegate;
+
+    public GlobalHandler(ILogger<ExceptionHandlerMiddleware> logger,RequestDelegate requestDelegate )
+    {
+        _logger = logger;
+        _requestDelegate = requestDelegate;
+    }
+
+    public async  Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _requestDelegate(context);
+        }
+        catch (Exception e)
+        {
+            var errorID = Guid.NewGuid;
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Response.ContentType = "application/json";
+            var erromsg = new
+            {
+                Id = errorID,
+                ErroMessage = "Something Went Wrong"
+            };
+            await context.Response.WriteAsJsonAsync(erromsg);
+
+        }
+    }
+}
